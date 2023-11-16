54ef48b29f7a2b8cb39ccf764973c9c5d778d7fc
Alexey <Alex_TNK_BP@mail.ru>
2023-11-16 20:40:21 +0300

    errors fixed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -29,28 +29,21 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int n = 0;
-
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid == uuid) {
-                n = i;
-
-            }
-            if (n != 0) {
-                System.arraycopy(storage, n + 1, storage, n, storage.length - 1);
+                System.arraycopy(storage, i + 1, storage, i, storage.length - 1);
                 storage[size] = null;
                 size = size - 1;
-
             }
-        }
 
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-      return   Arrays.copyOfRange(storage,0, size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
