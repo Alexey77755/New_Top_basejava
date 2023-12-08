0c72a47f9f127be11a54f28c489bc7997e7b1630
Alexey <Alex_TNK_BP@mail.ru>
2023-12-08 17:19:09 +0300

    SortedArrayStorage implemented

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -9,27 +9,50 @@ public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
     public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            System.out.println("Resume "  + r.getUuid() + " not exist");
 
+        } else {
+            storage[index] = r;
+        }
     }
 
     @Override
     public void save(Resume r) {
+        if (getIndex(r.getUuid()) > 0) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
 
+        } else if (size == STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+
+        } else {
+            storage[-(Arrays.binarySearch(storage,0,size, r)+1)] = r;
+            size++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
 
+        } else {
+            System.arraycopy(storage,index+1,storage,index,size - 1);
+            size--;
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
     @Override
     protected int getIndex(String uuid) {
